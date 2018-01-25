863e3d43ac4c47e671aadb4394b40f3049e301b9
Alan <slaklad@gmail.com>
2018-01-25 12:55:09 +0000

    Ultimas Alteracoes

--- themes/university-theme/single-event.php
+++ themes/university-theme/single-event.php
@@ -15,17 +15,13 @@
     
     <div class="container container--narrow page-section">
        <div class="metabox metabox--position-up metabox--with-home-link">
-          <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog')?>"><i class="fa fa-home" aria-hidden="true"></i> <?php ?> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('j/n/y'); ?> in <?php echo get_the_category_list(', ') ?></span></p>
+          <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> <?php ?> Events Home</a> <span class="metabox__main"><?php the_title();?></span></p>
         </div>
       
       <div class="generic-content"><?php the_content(); ?></div>
     </div>
-    
-    
-     
      
   <?php }
  
     get_footer();
 ?>
-
